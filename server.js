c16a0dd0a396f47d81054ef3c80b8289417ed1b3
Mahyarmadad <erfansam8891@yahoo.com>
2020-04-21 17:36:52 +0430

    Update Database

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const connectDB = require("./config/db");
 
+connectDB();
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.use("/api/users", require("./routes/users"));
